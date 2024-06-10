682fd5158ac060729c08e22c64678b2e9de49e08
Adarsh raj <rajadarsh232@gmail.com>
2024-06-10 11:50:29 +0000

    while loop use to draw a box

---
+++ draw_box.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int row=0;
+	puts("--------------------");
+	while(row<8)
+	{
+		puts("|                  |");
+		row++;
+	}
+	puts("--------------------");
+	return 0;
+}
